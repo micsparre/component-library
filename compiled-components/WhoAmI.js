c694ea31673945bfb2d6dfc7c5b71a32f72ad60e
Michael Sparre <michaelsparre@berkeley.edu>
2021-07-14 13:53:42 -0700

    second file

--- compiled-components/WhoAmI.js
+++ compiled-components/WhoAmI.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 export class TestComponent extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("div", null, "Hello World!");
+    return /*#__PURE__*/React.createElement("div", null, "Who am I?");
   }
 
 }
---
+++ components/WhoAmI.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class TestComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                Who am I?
+            </div>
+        )
+    }
+}
